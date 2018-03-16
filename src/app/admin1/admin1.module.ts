d495a9ec4638f632193ad24c1883a387b093b39a
Jordan Ta <jordanta@Jordans-MacBook-Pro.local>
2018-03-16 11:10:31 -0700

    my app first.

---
+++ src/app/admin1/admin1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Admin1RoutingModule } from './admin1-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Admin1RoutingModule
+  ],
+  declarations: []
+})
+export class Admin1Module { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { SiteHeaderComponent } from './site-header/site-header.component';
+import { AdminLinkDirective } from './admin-link.directive';
+import { ConvertToEuroPipe } from './convert-to-euro.pipe';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteHeaderComponent,
+    AdminLinkDirective,
+    ConvertToEuroPipe
   ],
   imports: [
     BrowserModule
